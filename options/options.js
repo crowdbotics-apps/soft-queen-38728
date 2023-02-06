4cd4dd04af30bed3b5f31a31500f56d9233d6b2e
Crowdbotics <team@crowdbotics.com>
2023-02-06 02:13:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-queen-38728",
+  url: "https://soft-queen-38728.botics.co",
+  api: "https://soft-queen-38728.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
